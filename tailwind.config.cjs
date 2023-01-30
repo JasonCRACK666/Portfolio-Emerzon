19effb6958d65c91ced30c5ad8ea9477d23995ae
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-01-29 22:06:30 -0500

    feat(tailwindcss): add font mplus and paddings for break points (responsive)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      padding: {
+        layout2xl: "24rem",
+        layoutxl: "16rem",
+        layoutlg: "4rem",
+        layoutmd: "0rem",
+        layoutsm: "0rem",
+      },
+      fontFamily: {
+        mplug: ["'M PLUS Rounded 1c'", "Verdana", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
